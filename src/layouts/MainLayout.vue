23befcb3208f52d98307ba9d9aa9e058d43aaa6f
Toeiiz <s5935512064@phuket.psu.ac.th>
2022-02-28 17:09:56 +0700

    set page layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,43 +1,131 @@
 <template>
   <q-layout view="hHh LpR lff">
-
-    <q-header elevated class="bg-primary text-white">
+    <q-header elevated class="bg-grey-9 text-white">
       <q-toolbar>
-        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+        <q-btn
+          dense
+          flat
+          round
+          icon="menu"
+          v-show="!leftDrawerOpen"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
 
-        <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-          </q-avatar>
-          Title
+        <q-toolbar-title class="q-py-sm">
+          <q-btn @click="$router.push('/')" flat>
+            <img
+              style="width: 200px; max-width: 80vw"
+              src="../assets/logo.png"
+            />
+          </q-btn>
         </q-toolbar-title>
+
+        <div class="q-gutter-x-xs">
+          <q-btn flat round dense icon="notifications" />
+          <q-btn flat round dense icon="mode_comment" />
+          <span>Hi, admin</span>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/avatar.png" />
+          </q-avatar>
+        </div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" behavior="desktop" bordered>
-      <!-- drawer content -->
+    <q-drawer
+      show-if-above
+      :breakpoint="500"
+      v-model="leftDrawerOpen"
+      side="left"
+      behavior="desktop"
+      bordered
+    >
+      <q-list separator>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
     </q-drawer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
-
   </q-layout>
 </template>
 
 <script>
-import { ref } from 'vue'
+import EssentialLink from "../components/EssentialLink.vue";
+import { useQuasar } from "quasar";
+import { ref } from "vue";
 
 export default {
-  setup () {
-    const leftDrawerOpen = ref(false)
+  data() {
+    return {
+      leftDrawer: ref(false),
+    };
+  },
+
+  components: {
+    EssentialLink,
+  },
+  setup() {
+    const $q = useQuasar();
+    const linksList = [
+      {
+        title: "Overview",
+        icon: "cloud_queue",
+        link: "/",
+        useMethod: "",
+      },
+      {
+        title: "Report",
+        icon: "description",
+        link: "/",
+        useMethod: "",
+      },
+
+      {
+        title: "Map",
+        icon: "map",
+        link: "/",
+        useMethod: "",
+      },
+      {
+        title: "Location",
+        icon: "explore",
+        link: "/",
+        useMethod: "",
+      },
+      {
+        title: "Device Management",
+        icon: "format_list_bulleted",
+        link: "/",
+        useMethod: "",
+      },
+      {
+        title: "Manage Work",
+        icon: "format_list_bulleted",
+        link: "/",
+        useMethod: "",
+      },
+      {
+        title: "Setting",
+        icon: "format_list_bulleted",
+        link: "/",
+        useMethod: "",
+      },
+    ];
+    const leftDrawerOpen = ref(true);
 
     return {
+      essentialLinks: linksList,
       leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
-  }
-}
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+    $q.platform.is.mobile;
+  },
+};
 </script>
