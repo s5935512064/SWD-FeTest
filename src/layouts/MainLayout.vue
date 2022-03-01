637b38a3599597134b1a27ff344351fde25f132d
Toeiiz <s5935512064@phuket.psu.ac.th>
2022-03-01 21:48:08 +0700

    dashboard and navbar

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -11,13 +11,29 @@
           @click="leftDrawerOpen = !leftDrawerOpen"
         />
 
-        <q-toolbar-title class="q-py-sm">
+        <q-toolbar-title class="row items-center q-py-sm q-gutter-x-md">
           <q-btn @click="$router.push('/')" flat>
             <img
               style="width: 200px; max-width: 80vw"
               src="../assets/logo.png"
             />
           </q-btn>
+          <q-input
+            style="max-width: 350px"
+            outlined
+            dense
+            bg-color="white"
+            color="black"
+            class="border-none outline-none q-pl-xl"
+            placeholder="Search Here"
+          >
+            <template v-slot:append>
+              <q-icon name="search" />
+            </template>
+          </q-input>
+          <q-icon name="format_list_bulleted" />
+          <q-icon name="dashboard" />
+          <q-icon name="map" />
         </q-toolbar-title>
 
         <div class="q-gutter-x-xs">
@@ -77,12 +93,16 @@ export default {
         icon: "cloud_queue",
         link: "/",
         useMethod: "",
+        level: 0,
+        children: [],
       },
       {
         title: "Report",
         icon: "description",
         link: "/",
         useMethod: "",
+        level: 0,
+        children: [],
       },
 
       {
@@ -90,30 +110,128 @@ export default {
         icon: "map",
         link: "/",
         useMethod: "",
+        level: 0,
+        children: [],
       },
       {
         title: "Location",
         icon: "explore",
         link: "/",
         useMethod: "",
+        level: 0,
+        children: [],
       },
       {
         title: "Device Management",
         icon: "format_list_bulleted",
         link: "/",
         useMethod: "",
+        level: 0,
+        children: [
+          {
+            title: "Sensor",
+            icon: "signal_cellular_alt",
+            link: "/",
+            level: 1,
+            children: [],
+          },
+          {
+            title: "Gateway",
+            icon: "router",
+            link: "/",
+            level: 1,
+            children: [],
+          },
+          {
+            title: "Camera",
+            icon: "photo_camera",
+            link: "/",
+            level: 1,
+            children: [],
+          },
+        ],
       },
       {
         title: "Manage Work",
         icon: "format_list_bulleted",
         link: "/",
         useMethod: "",
+        level: 0,
+        children: [
+          {
+            title: "Work Management",
+            icon: "work",
+            link: "/",
+            level: 1,
+            children: [],
+          },
+          {
+            title: "Work Status",
+            icon: "badge",
+            link: "/",
+            level: 1,
+            children: [],
+          },
+        ],
       },
       {
         title: "Setting",
         icon: "format_list_bulleted",
         link: "/",
         useMethod: "",
+        level: 0,
+        children: [
+          {
+            title: "Alert",
+            icon: "info",
+            link: "/",
+            level: 1,
+            children: [],
+          },
+          {
+            title: "Network",
+            icon: "wifi",
+            link: "/",
+            level: 1,
+            children: [],
+          },
+          {
+            title: "Location Setting",
+            icon: "explore",
+            link: "/",
+            level: 1,
+            children: [],
+          },
+          {
+            title: "User",
+            icon: "person",
+            link: "/",
+            level: 1,
+            children: [],
+          },
+          {
+            title: "Permission Setting",
+            icon: "manage_accounts",
+            link: "/",
+            level: 1,
+            children: [],
+          },
+          {
+            title: "Form",
+            icon: "description",
+            link: "/",
+            level: 1,
+            children: [],
+          },
+        ],
+      },
+      {
+        title: "Logout",
+        icon: "logout",
+        link: "/",
+        useMethod: "",
+        level: 0,
+        children: [],
       },
     ];
     const leftDrawerOpen = ref(true);
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,34 @@
 <template>
-  <q-page class="flex flex-center"> </q-page>
+  <div class="maincontainer">
+    <q-page class="column q-pa-md">
+      <div class="q-pa-md">
+        <div class="row justify-between">
+          <Card />
+        </div>
+      </div>
+      <div class="col">
+        <Table />
+      </div>
+    </q-page>
+  </div>
 </template>
 
 <script>
 import { defineComponent } from "vue";
+import Card from "../components/Card.vue";
+import Table from "../components/Table.vue";
 
-export default defineComponent({
-  name: "PageIndex",
-});
+export default {
+  components: {
+    Card,
+    Table,
+  },
+};
 </script>
+
+<style lang="scss" scoped>
+.maincontainer {
+  width: 80vw;
+  height: 100vh;
+}
+</style>
